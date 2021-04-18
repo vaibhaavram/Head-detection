c4fa48e09900492180d67859e24f3a7854ac5e3f
vaibhaavram <68799245+vaibhaavram@users.noreply.github.com>
2021-04-18 12:42:38 +0530

    Add files via upload

---
+++ rasp.py
@@ -0,0 +1,123 @@
+
+import cv2
+import numpy as np
+import glob
+import random
+import imutils
+from imutils.video import VideoStream
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+# GPIO.setup(22,GPIO.OUT)
+# GPIO.setup(5,GPIO.OUT)
+# Load Yolo
+net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_testing.cfg")
+
+# Name custom object
+classes = ["person"]
+
+# Images path
+images_path = glob.glob(r"D:\YOLO image detection\custom data set classroom/*.jpg")
+
+
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Insert here the path of your images
+random.shuffle(images_path)
+# loop through all the images
+# vs = VideoStream(src="rtsp://admin:UKQNXJ@192.168.43.98/").start()
+vs = VideoStream(0).start()
+time.sleep(2.0)
+while True:
+	# Loading image
+	frame = vs.read()
+	l = int(frame.shape[0])
+	b = int(frame.shape[1])
+	
+	# frames = [frame[0:int(l/2), 0:int(b/2), :], frame[0:int(l/2), int(b/2):int(b), :], frame[int(l/2):int(l), 0:int(b/2), :],frame[int(l/2):int(l), int(b/2):int(b), :]]
+	frames = [frame[:, 0:int(b/2), :], frame[:, int(b/2):int(b), :]]
+
+	list_detect = [0]*2
+	for j, frame in enumerate(frames):
+		img = cv2.resize(frame, None, fx=0.4, fy=0.4)
+		height, width, channels = img.shape
+
+		# Detecting objects
+		blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+		net.setInput(blob)
+		outs = net.forward(output_layers)
+
+		# Showing informations on the screen
+		class_ids = []
+		confidences = []
+		boxes = []
+		for out in outs:
+			for detection in out:
+				scores = detection[5:]
+				class_id = np.argmax(scores)
+				confidence = scores[class_id]
+				if confidence > 0.5:
+					# Object detected
+					# print(class_id)
+					list_detect[j] = 1
+
+					center_x = int(detection[0] * width)
+					center_y = int(detection[1] * height)
+					w = int(detection[2] * width)
+					h = int(detection[3] * height)
+
+					# Rectangle coordinates
+					x = int(center_x - w / 2)
+					y = int(center_y - h / 2)
+
+					boxes.append([x, y, w, h])
+					confidences.append(float(confidence))
+					class_ids.append(class_id)
+
+		indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+		# print(indexes)
+		font = cv2.FONT_HERSHEY_PLAIN
+		for i in range(len(boxes)):
+			if i in indexes:
+				x, y, w, h = boxes[i]
+				label = str(classes[class_ids[i]])
+				color = colors[class_ids[i]]
+				cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+				# cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
+				img = cv2.resize(img, (450,450))
+				cv2.imshow("Image number {}".format(j), img)
+				cv2.waitKey(1)
+		img = cv2.resize(img, (450,450))
+		cv2.imshow("Image number {}".format(j), img)
+		cv2.waitKey(1)
+	print(list_detect)
+	time.sleep(1)
+	if((list_detect[0] == 1) and (list_detect[1] == 1)):
+
+		print("BOTH")
+		GPIO.output(17,GPIO.HIGH)
+		GPIO.output(27,GPIO.HIGH)
+		# GPIO.output(22,GPIO.HIGH)
+		# GPIO.output(5,GPIO.HIGH)
+	elif((list_detect[0] == 0) and (list_detect[1] == 1)):
+		print("FRAME 1")
+		GPIO.output(17,GPIO.HIGH)
+
+	else:
+		print("Frame 2")
+		GPIO.output(27,GPIO.HIGH)
+	time.sleep(1)
+
+    
+
+
+cv2.destroyAllWindows()
+# vs.stop()
+
